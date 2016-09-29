a5b9c67dff73353f9778e121ddc7ad7ecf3b3770
Alexis <alxpequeno@gmail.com>
2016-09-29 18:45:10 -0500

    primera fase gui

---
+++ Cine/src/gui/Menu.java
@@ -0,0 +1,96 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.UIManager;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Menu extends JFrame {
+
+	private JPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu mnMantenimiento;
+	private JMenu mnRegistroDeReserva;
+	private JMenu mnControlDePago;
+	private JMenu mnReportes;
+	private JMenuItem mntmEmpleados;
+	private JMenuItem mntmClientes;
+	private JMenuItem mntmCines;
+	private JMenuItem mntmSalas;
+	private JMenuItem mntmPeliculas;
+	private JMenuItem mntmFunciones;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Menu() {
+		setTitle("Cinematec - Men\u00FA Principal");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnMantenimiento = new JMenu("Mantenimiento");
+		menuBar.add(mnMantenimiento);
+		
+		mntmEmpleados = new JMenuItem("Empleados");
+		mnMantenimiento.add(mntmEmpleados);
+		
+		mntmClientes = new JMenuItem("Clientes");
+		mnMantenimiento.add(mntmClientes);
+		
+		mntmCines = new JMenuItem("Cines");
+		mnMantenimiento.add(mntmCines);
+		
+		mntmSalas = new JMenuItem("Salas");
+		mnMantenimiento.add(mntmSalas);
+		
+		mntmPeliculas = new JMenuItem("Peliculas");
+		mnMantenimiento.add(mntmPeliculas);
+		
+		mntmFunciones = new JMenuItem("Funciones");
+		mnMantenimiento.add(mntmFunciones);
+		
+		mnRegistroDeReserva = new JMenu("Registro de Reserva");
+		menuBar.add(mnRegistroDeReserva);
+		
+		mnControlDePago = new JMenu("Control de Pago");
+		menuBar.add(mnControlDePago);
+		
+		mnReportes = new JMenu("Reportes");
+		menuBar.add(mnReportes);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+}
